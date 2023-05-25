b003254ae37947f06f69791a6387b6b24186423a
MarjitaSaha <103816611+MarjitaSaha@users.noreply.github.com>
2023-05-25 20:56:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -30,6 +30,6 @@ for (let index = 0; index < encodedStr.length; index++) {
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
+console.log(rot13("SERR YBIR? NPPVBWBO"));
 
 module.exports = rot13;
